d995e89f1eb12c787ffd34e6db82e1902e7f1abe
zoecki187 <nico.prell@mail.de>
2022-11-08 17:01:28 +0100

    anderer mail user test gefüllte datenbank

--- src/main/java/com/project/MappingController.java
+++ src/main/java/com/project/MappingController.java
@@ -117,7 +117,7 @@ public class MappingController {
             try {
 
                 // hier Präferenz holen
-                String nutzerPraef = PostgresDataManager.getPostgresDataManger().getLiebVerein("timo.werner@gmx.de").getVerein();
+                String nutzerPraef = PostgresDataManager.getPostgresDataManger().getLiebVerein("nico.prell@mail.de").getVerein();
                 // hier ausgeben mit Alexa
                 outText.append(nutzerPraef);
                 //outText.append("1860 bis in den Tod! ");
